94b7def96f77c1c621f4ff5b7c1de97870053b55
liulu17 <1085554710@qq.com>
2022-11-02 21:58:51 +0800

    fix

--- boot/build.sh
+++ boot/build.sh
@@ -1,4 +1,5 @@
 as -32 boot.s -o boot.o
-ld -m elf_i386 -Ttext 0 -o boot.bin boot.o
-objcopy -O binary boot.bin
+# ld -m elf_i386 -Ttext 0 -o boot.bin boot.o
+ld -m elf_i386 --oformat binary -N -Ttext 0x0 -o boot1.bin boot.o
+# objcopy -O binary boot.bin
 git add . && git commit -m "fix" && git push 
