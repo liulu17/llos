f632ce80cbf3d6665efee911ecfae6c0601296c5
liulu17 <1085554710@qq.com>
2022-11-02 21:51:26 +0800

    fix

--- boot/boot.s
+++ boot/boot.s
@@ -1,56 +1,3 @@
-//  .code16
-//     .global _start
-//     .text
-
-//     .equ BOOTSEG, 0x07c0
-   
-
-//     ljmp $BOOTSEG,$_start
-
-// _start:
-//     mov $BOOTSEG,%ax  
-//     mov %ax,%es        
-//     mov $0x03,%ah      
-//     xor %bh,%bh        
-//     int $0x10
-
-//     mov     $20,%cx    
-//     mov     $0x0007,%bx
-   
-//     mov     $msg1,%bp
-//     mov     $0x1301,%ax
-//     int     $0x10  
-//     movw $0xb800,%ax
-//     movw %ax,%es
-//     // movw %ax,%di
-//     xorw %di,%di
-//     xor %ecx,%ecx
-//     movb $26,%cx
-//     xor %eax,%eax
-//     movb $65,%al
-//     movb $0xc,%ah
-// loop1:
-//     movw %ax,%es:(%di)
-//     inc %al
-//     add $2,%di
-//     loop loop1
-
-
-// loop_forever:          
-//     jmp loop_forever
-
-// sectors:
-//     .word 0
-
-// msg1:
-//     .byte 13,10
-//     .ascii "Hello world!"
-//     .byte 13,10,13,10
-
-//     .=0x1fe
-// boot_flag:
-//     .word 0xAA55
-
 .code16
     .equ INITSEG,0x9000
     .equ SETUPSEG, 0x9020
@@ -90,7 +37,13 @@ clear_creen:
     int $0x10
 
 print_str:
+// ah=0x13 功能号
+// al=0x01 写方式
+// cx = 字符串长度
+// DH/DL 起始行列
+// BH/BL 页号、属性
     movw $0x1301,%ax
+    movw $0x0,%dx
     movw $msg,%bp
     movw $22,%cx
     movw $0x0007,%bx
